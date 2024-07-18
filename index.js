dd5879c7de912bc3209be87fa3dd6b959e0d05c8
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-18 15:29:10 +0300

    complete task_01

---
+++ index.js
@@ -0,0 +1,24 @@
+// Первый аргумент - массив, второй - колбек-функция, которая применяется на массив
+// Колбек-функция применяется на каждый элемент массива и проверяет условие
+// Если хотя бы один раз колбек-функция вернула true, то any возвращает true
+// Иначе - false
+
+// Если колбек-функция не была передана, то из any возвращаем true, 
+// если в массиве есть хотя бы один элемент, который дает true,
+// если все значения в массиве дают false, то возвращаем false
+
+// console.log(any([0, 1, 2, 0], x => x >= 2)); -> true
+// console.log(any([0, 0, 1, 0])); -> true
+// console.log(any([0, 0, 0, 0])); -> false
+
+
+function any(array, collback = (x) => !!x === true) {
+  return array.filter(collback).length > 0;
+}
+
+console.log(any([0, 1, 2, 0], (x) => x >= 2)); // -> true
+console.log(any([0, 1, 1, 0], (x) => x >= 2)); // -> false
+console.log(any([0, 0, 1, 0])); //-> true
+console.log(any([0, 0, 0, 0])); //-> false
+console.log(any([undefined, "", null, 0, NaN])); //-> false
+console.log(any([undefined, "", null, 0, NaN, []])); //-> true
