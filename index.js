4541504f56a9b143b0c45836e6630d0c5d37f8b6
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-19 15:36:49 +0300

    add algorithm complexity

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
 
+//O(1)
 function sumTo(n) {
   const last = 1 + (n - 1); // formula for the sum of the first n numbers of an arithmetic progression in increments of 1
   return ((1 + last) / 2) * n;
 }
 
+//O(n)
 function sumTo(n) {
   [currValue, sum] = [0, 0];
 
