c882ecb7d9f8eef1a78e99c02bf1cbe0d5dc87ce
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-21 20:32:36 +0300

    refactor task_05

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 // Реализовать функцию without.
 // Первый аргумент - массив, второй и последующие - значения
-
 // Функция возвращает новый массив, который наполнен теми значениями,
 // которые не передавались как второй и последующие аргументы функции
 
-// console.log(without([2, 1, 2, 3], 1, 2)) -> [3]
-// console.log(without([2, 1, 10, 20, 5], 1, 2, 5)) -> [10, 20]
-
 //O(2n)
 function without(array, ...rest) {
-  const hash = rest.reduce((accum, el) => {
+  const subtracted = rest.reduce((accum, el) => {
     accum[el] = el;
     return accum;
   }, {});
 
-  return array.filter((el) => !hash[el]);
+  return array.filter((el) => !subtracted[el]);
 }
 
 console.log(without([2, 1, 2, 3], 1, 2)); //-> [3]
