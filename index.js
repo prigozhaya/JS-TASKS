fccac31f5fa95995979eda4e674e51c176963c5b
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-21 20:26:15 +0300

    refactor task_02

--- index.js
+++ index.js
@@ -2,17 +2,29 @@
 // Возвращает новый массив, который состоит только из тех элементов,
 // которые встретились в одном массиве, но не встретились в другом
 
-// console.log(arrayDiff([1, 2, 3], [1, 2, 4])); -> [3, 4]
-// console.log(arrayDiff([1, 3, 3, 4], [1, 3, '4'])); -> [4, '4']
-
-
+//O(2n^2)
 function arrayDiff(array1, array2) {
-  const arr1 = array1.filter((el) => !array2.includes(el));
-  const arr2 = array2.filter((el) => !array1.includes(el));
-
-  return arr1.concat(arr2);
+  return array1
+    .filter((el) => !array2.includes(el))
+    .concat(array2.filter((el) => !array1.includes(el)));
 }
 
+//O(4n)
+function arrayDiff(array1, array2) {
+  const map1 = array1.reduce((accum, el) => {
+    accum.set(el, el);
+    return accum;
+  }, new Map());
+
+  const map2 = array2.reduce((accum, el) => {
+    accum.set(el, el);
+    return accum;
+  }, new Map());
+
+  return array1
+    .filter((el) => !map2.has(el))
+    .concat(array2.filter((el) => !map1.has(el)));
+}
 
 console.log(arrayDiff([1, 2, 3], [1, 2, 4])); //-> [3, 4]
 console.log(arrayDiff([1, 3, 3, 4], [1, 3, "4"])); // -> [4, '4']
