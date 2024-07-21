d75e67b9368e3b317e784701427a825cff86bd86
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-21 20:40:53 +0300

    refactor task_13

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
 
-// Например:
 
-// let arr = [5, 3, 8, 1];
-
-// let filtered = filterRange(arr, 1, 4);
-
-// alert( filtered ); // 3,1
-
+//O(n)
 function filterRange(arr, bottomLine, topLine) {
   return arr.filter((el) => el >= bottomLine && el <= topLine);
 }
