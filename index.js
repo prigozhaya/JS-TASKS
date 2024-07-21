c915d7ba82008137a4968eddacfa112cb17249cd
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-21 20:37:48 +0300

    refactor task_09

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const obj = {
   id: 0,
-  name: 'Obj-name',
-  
-  toString(){
-    return this.name
+  name: "Obj-name",
+
+  toString() {
+    return this.name;
   },
 
-  valueOf(){
-    return this.id
-  }
+  valueOf() {
+    return this.id;
+  },
 };
 
-
-console.log(`Name: ${obj}`); 		// Name: Obj-name
+console.log(`Name: ${obj}`); // Name: Obj-name
 console.log(+obj); // 0
 console.log(obj + 10); // 10
