e298409b4ee5325c91eaa2086bb66baabc7c186e
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-21 20:30:05 +0300

    refactor task_04

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // Функция принимает 2 массива, и возвращает массив объединенных значений,
 // без дублирования
 
-// console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); -> [5, 1, 2, 3, 4]
-// console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); -> [5, 1, 3, 4]
 
 //O(3n)
-function union(arr1, arr2){
-  return [...new Set(arr1.concat(arr2))]
- }
- 
- console.log(union([5, 1, 2, 3, 3], [4, 3, 2]));// -> [5, 1, 2, 3, 4]
- console.log(union([5, 1, 3, 3, 4], [1, 3, 4]));// -> [5, 1, 3, 4]
+function union(arr1, arr2) {
+  return [...new Set(arr1.concat(arr2))];
+}
+
+console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); // -> [5, 1, 2, 3, 4]
+console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); // -> [5, 1, 3, 4]
