3516ea7449557025249ef4134e13bd95678575e9
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-23 16:18:25 +0300

    add avoiding symbols

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 //O(2n)
 function getLengthOfShortestWord(str) {
-  return str
+  return str.replace (/[^a-zA-Zа-яА-Я0-9 ]+/g, '')
     .split(" ")
     .reduce((acc, curr) => (acc = curr.length < acc.length ? curr : acc))
     .length;
@@ -12,7 +12,7 @@ function getLengthOfShortestWord(str) {
 
 //O(3n)
 function getLengthOfShortestWord(str) {
-  return Math.min(...str.split(" ").map((word) => word.length));
+  return Math.min(...str.replace (/[^a-zA-Zа-яА-Я0-9 ]+/g, '').split(" ").map((word) => word.length));
 }
 console.log(getLengthOfShortestWord("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")) 	// => 2
-console.log(getLengthOfShortestWord("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incididunt labore dolore magna aliqua.")) 	// => 3
+console.log(getLengthOfShortestWord("Lorem ipsum dolor sit amet, a, consectetur adipiscing elit, sed eiusmod tempor incididunt labore dolore magna aliqua.")) 	// => 2
