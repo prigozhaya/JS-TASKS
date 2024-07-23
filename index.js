b7043f39fb10b5e5554fa6ef4d73595b75fac4ac
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2024-07-23 13:38:50 +0300

    refactor arrayDiff function

--- index.js
+++ index.js
@@ -11,20 +11,17 @@ function arrayDiff(array1, array2) {
 
 //O(4n)
 function arrayDiff(array1, array2) {
-  const map1 = array1.reduce((accum, el) => {
-    accum.set(el, el);
-    return accum;
-  }, new Map());
-
-  const map2 = array2.reduce((accum, el) => {
-    accum.set(el, el);
-    return accum;
-  }, new Map());
-
-  return array1
-    .filter((el) => !map2.has(el))
-    .concat(array2.filter((el) => !map1.has(el)));
-}
+  const set1 = new Set(array1);
+  const set2 = new Set(array2);
+  
+    return array1
+      .filter((el) => !set2.has(el))
+      .concat(array2.filter((el) => !set1.has(el)));
+  }
+  
+  console.log(arrayDiff([1, 2, 3], [1, 2, 4])); //-> [3, 4]
+  console.log(arrayDiff([1, 3, 3, 4], [1, 3, "4"])); // -> [4, '4']
+  console.log(arrayDiff([1, 3, 3, 4, 5], [1, 3, 3, 3, "4"])); // -> [4, 5,'4']
 
 console.log(arrayDiff([1, 2, 3], [1, 2, 4])); //-> [3, 4]
 console.log(arrayDiff([1, 3, 3, 4], [1, 3, "4"])); // -> [4, '4']
